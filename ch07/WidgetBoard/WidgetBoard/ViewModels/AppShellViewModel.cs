f4fc1cb67541f4257111bee11cdcd2440d9d535c
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2023-01-06 14:56:53 -0500

    Add files via upload

---
+++ ch07/WidgetBoard/WidgetBoard/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using WidgetBoard.Models;
+
+namespace WidgetBoard.ViewModels;
+
+public class AppShellViewModel : BaseViewModel
+{
+    private Board currentBoard;
+
+    public Board CurrentBoard
+    {
+        get => currentBoard;
+        set
+        {
+            if (SetProperty(ref currentBoard, value))
+            {
+                BoardSelected(value);
+            }
+        }
+    }
+
+    public AppShellViewModel()
+    {
+        Boards.Add(
+            new Board
+            {
+                Name = "My first board",
+                Layout = new FixedLayout
+                {
+                    NumberOfColumns = 3,
+                    NumberOfRows = 2
+                }
+            });
+    }
+
+    public ObservableCollection<Board> Boards { get; } = new ObservableCollection<Board>();
+
+    private async void BoardSelected(Board board)
+    {
+        await Shell.Current.GoToAsync(
+            "fixedboard",
+            new Dictionary<string, object>
+            {
+            { "Board", board}
+            });
+    }
+
+}
