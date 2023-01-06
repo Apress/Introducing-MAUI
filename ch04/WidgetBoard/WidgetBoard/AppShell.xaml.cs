26e2040d4da73eabaca17efc304f8981790487e7
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2023-01-06 14:54:36 -0500

    Add files via upload

---
+++ ch04/WidgetBoard/WidgetBoard/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WidgetBoard;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ch04/WidgetBoard/WidgetBoard/ViewModels/ClockWidgetViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WidgetBoard.ViewModels;
+
+public class ClockWidgetViewModel : BaseViewModel, IWidgetViewModel
+{
+    private readonly Scheduler scheduler = new();
+    private DateOnly date;
+    private TimeOnly time;
+
+    public ClockWidgetViewModel()
+    {
+        SetTime(DateTime.Now);
+    }
+
+    public DateOnly Date
+    {
+        get => date;
+        set => SetProperty(ref date, value);
+    }
+
+    public TimeOnly Time
+    {
+        get => time;
+        set => SetProperty(ref time, value);
+    }
+
+    private void SetTime(DateTime dateTime)
+    {
+        Date = DateOnly.FromDateTime(dateTime);
+        Time = TimeOnly.FromDateTime(dateTime);
+
+        scheduler.ScheduleAction(
+            TimeSpan.FromSeconds(1),
+            () =>
+            {
+                SetTime(DateTime.Now);
+            });
+    }
+
+}
