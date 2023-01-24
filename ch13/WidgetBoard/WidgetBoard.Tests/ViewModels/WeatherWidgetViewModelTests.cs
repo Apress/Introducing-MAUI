d38fd9aa1a88530e1c8f69ba536fed2883d1ce43
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2023-01-24 14:53:23 -0500

    Add files via upload

---
+++ ch13/WidgetBoard/WidgetBoard.Tests/ViewModels/WeatherWidgetViewModelTests.cs
@@ -0,0 +1,67 @@
+﻿using WidgetBoard.Tests.Mocks;
+using WidgetBoard.ViewModels;
+
+namespace WidgetBoard.Tests.ViewModels;
+
+public class WeatherWidgetViewModelTests
+{
+    [Fact]
+    public async Task NullLocationResultsInPermissionErrorState()
+    {
+        var viewModel = new WeatherWidgetViewModel(
+            MockWeatherForecastService.ThatReturnsNoForecast(after: TimeSpan.FromSeconds(5)),
+            MockLocationService.ThatReturnsNoLocation(after: TimeSpan.FromSeconds(2)));
+
+        await viewModel.InitializeAsync();
+
+        Assert.Equal(State.PermissionError, viewModel.State);
+        Assert.Null(viewModel.Weather);
+    }
+
+    [Fact]
+    public async Task NullForecastResultsInErrorState()
+    {
+        var viewModel = new WeatherWidgetViewModel(
+            MockWeatherForecastService.ThatReturnsNoForecast(after: TimeSpan.FromSeconds(5)),
+            MockLocationService.ThatReturns(new Location(0.0, 0.0), after: TimeSpan.FromSeconds(2)));
+
+        await viewModel.InitializeAsync();
+
+        Assert.Equal(State.Error, viewModel.State);
+        Assert.Null(viewModel.Weather);
+    }
+
+    [Fact]
+    public async Task ValidForecastResultsInSuccessfulLoad()
+    {
+        var weatherForecastService = MockWeatherForecastService.ThatReturns(
+                new Communications.Forecast
+                {
+                    Current = new Communications.Current
+                    {
+                        Temperature = 18.0,
+                        Weather = new Communications.Weather[]
+                        {
+                            new Communications.Weather
+                            {
+                                Icon = "abc.png",
+                                Main = "Sunshine"
+                            }
+                        }
+                    }
+                },
+                after: TimeSpan.FromSeconds(5));
+        var locationService = MockLocationService.ThatReturns(
+                new Location(0.0, 0.0),
+                after: TimeSpan.FromSeconds(2));
+
+        var viewModel = new WeatherWidgetViewModel(
+            weatherForecastService,
+            locationService);
+
+        await viewModel.InitializeAsync();
+
+        Assert.Equal(State.Loaded, viewModel.State);
+        Assert.Equal("Sunshine", viewModel.Weather);
+    }
+}
---
+++ ch13/WidgetBoard/WidgetBoard.Tests/Views/ClockWidgetViewTests.cs
@@ -0,0 +1,21 @@
+﻿using WidgetBoard.Tests.Mocks;
+using WidgetBoard.Views;
+
+namespace WidgetBoard.Tests.Views;
+
+public class ClockWidgetViewTests
+{
+    [Fact]
+    public void TextIsUpdatedByTimeProperty()
+    {
+        var time = new DateTime(2022, 01, 01);
+
+        var clockWidget = new ClockWidgetView();
+
+        Assert.Null(clockWidget.Text);
+
+        clockWidget.WidgetViewModel = new MockClockWidgetViewModel(time);
+
+        Assert.Equal(time.ToString(), clockWidget.Text);
+    }
+}
